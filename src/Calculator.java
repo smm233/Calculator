ec7bee2ddd4a34449b5b90c8b8d060005e6b70a3
shil-mil <shyshy122303@gmail.com>
2025-03-03 22:34:06 +0800

    Create calculator frame

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,5 +5,17 @@ import javax.swing.*;
 import javax.swing.border.LineBorder;
 
 public class Calculator {
+    int boardWidth = 360;
+    int boardHeight = 540;
 
+    JFrame frame = new JFrame("Calculator");
+
+    Calculator() {
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+    }
 }
